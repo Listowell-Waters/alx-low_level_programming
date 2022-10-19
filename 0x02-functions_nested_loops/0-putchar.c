263631abef6aba2c2f201df29075bbbf3477cd7c
Listowell Appiah <listowellappiah1@gmail.com>
2022-10-19 14:59:45 -0700

    A step at a time

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - This function prints a string
+ * Author: Listowell Appiah
+ * Return: Always return 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
